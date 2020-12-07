fb0d3cab5e78ef0dd19da4d79dbf622ea95e8538
KishoreKaushal <kshr4kshl@gmail.com>
2020-12-07 15:51:15 +0530

    Initializing the member variables.

--- XMLPropertyTree.cpp
+++ XMLPropertyTree.cpp
@@ -4,13 +4,22 @@
 
 #include "XMLPropertyTree.h"
 
-XMLPropertyTree::XMLElement::XMLElement(const XMLCh *localname,
-                                        const XMLCh *qname,
-                                        const XMLCh *uri,
-                                        const XMLCh *data,
-                                        const xercesc::Attributes &attributes)
-                                        noexcept {
+XMLPropertyTree::XMLElement::XMLElement(const XMLCh *arguri,
+                                        const XMLCh *arglocalname,
+                                        const XMLCh *argqname,
+                                        const XMLCh *argdata,
+                                        const xercesc::Attributes &argattributes)
+                                        noexcept:
+                                            uri(XString(arguri)()),
+                                            localname(XString(arglocalname)()),
+                                            qname(XString(argqname)()),
+                                            data(XString(argdata)()) {
 
+    // Initializing attribute map
+    XMLSize_t numattr = argattributes.getLength();
 
+    for (XMLSize_t i = 0; i < numattr; i++) {
+        mattr.insert(std::make_pair(qname, XString(argattributes.getValue(i))()));
+    }
 
 }
